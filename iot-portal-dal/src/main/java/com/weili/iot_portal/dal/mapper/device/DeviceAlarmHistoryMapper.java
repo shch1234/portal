d50de693e477b8f3d7ebd2661eb3721e72afe8db
luying <luying@weili.com>
2025-12-25 13:56:21 +0800

    设备报警管理

--- iot-portal-dal/src/main/java/com/weili/iot_portal/dal/mapper/device/DeviceAlarmHistoryMapper.java
+++ iot-portal-dal/src/main/java/com/weili/iot_portal/dal/mapper/device/DeviceAlarmHistoryMapper.java
@@ -2,34 +2,10 @@ package com.weili.iot_portal.dal.mapper.device;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.weili.iot_portal.dal.dataobject.device.DeviceAlarmHistoryDO;
-import com.weili.iot_portal.domain.device.resp.AlarmManageRespVO;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 @Mapper
 public interface DeviceAlarmHistoryMapper extends BaseMapper<DeviceAlarmHistoryDO> {
-
-    /**
-     * 查询报警管理列表总数
-     */
-    Long countAlarmManageList(@Param("deviceCode") String deviceCode,
-                              @Param("deviceType") String deviceType,
-                              @Param("isActive") Integer isActive,
-                              @Param("startTime") Long startTime,
-                              @Param("endTime") Long endTime);
-
-    /**
-     * 查询报警管理列表（分页）
-     */
-    List<AlarmManageRespVO> selectAlarmManageList(@Param("deviceCode") String deviceCode,
-                                                    @Param("deviceType") String deviceType,
-                                                    @Param("isActive") Integer isActive,
-                                                    @Param("startTime") Long startTime,
-                                                    @Param("endTime") Long endTime,
-                                                    @Param("offset") Integer offset,
-                                                    @Param("limit") Integer limit);
 }
 
 
