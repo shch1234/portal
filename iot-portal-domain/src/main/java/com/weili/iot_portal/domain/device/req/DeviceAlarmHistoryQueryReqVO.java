617679094ae6937453d0655daaa9535b689de251
luying <luying@weili.com>
2025-12-30 16:47:33 +0800

    1

--- iot-portal-domain/src/main/java/com/weili/iot_portal/domain/device/req/DeviceAlarmHistoryQueryReqVO.java
+++ iot-portal-domain/src/main/java/com/weili/iot_portal/domain/device/req/DeviceAlarmHistoryQueryReqVO.java
@@ -1,7 +1,9 @@
 package com.weili.iot_portal.domain.device.req;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.weili.basic.common.model.PageParam;
+import com.weili.iot_portal.common.serializer.TimestampLongDeserializer;
 import com.weili.iot_portal.common.serializer.TimestampLongSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -25,10 +27,10 @@ public class DeviceAlarmHistoryQueryReqVO extends PageParam {
     private Integer isActive;
 
     @Schema(description = "开始时间（yyyy-MM-dd HH:mm:ss格式）", example = "2024-11-13 08:00:00")
-    @JsonSerialize(using = TimestampLongSerializer.class)
+    @JsonDeserialize(using = TimestampLongDeserializer.class)
     private Long startTime;
 
     @Schema(description = "结束时间（yyyy-MM-dd HH:mm:ss格式）", example = "2024-11-13 18:00:00")
-    @JsonSerialize(using = TimestampLongSerializer.class)
+    @JsonDeserialize(using = TimestampLongDeserializer.class)
     private Long endTime;
 }
